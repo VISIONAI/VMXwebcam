dd4103150c51b77f5bd8035c5e410818be4f4af7
Tomasz Malisiewicz <tom@vision.ai>
2015-03-14 05:04:13 -0400

    add random number to temp file names

--- VMXwebcam.go
+++ VMXwebcam.go
@@ -1,7 +1,7 @@
 // Here is a GO VMX driver which wraps the VMX process with an HTTP handler
 // This file is part of VMX
 // Author: Tomasz Malisiewicz
-// Copyright vision.ai, LLC 2014
+// Copyright vision.ai, LLC 2014-2015
 
 package main
 import "os/exec"
@@ -12,8 +12,9 @@ import "net/http"
 import "os"
 import "net"
 import "strconv"
+
 import "path/filepath"
-//import "github.com/gorilla/handlers"
+import "math/rand"
 
 func main() {
 
@@ -26,8 +27,7 @@ func main() {
 	cur_dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		log.Fatal(err)
-	}
-	
+	}	
 
         my_channel := make(chan int, 1)
 	
@@ -39,7 +39,7 @@ func main() {
 	fmt.Fprintf(os.Stderr,"Welcome to VMXcamserver, to get an image visit the following address:\n")
 	fmt.Printf("%s\n",addy)
 	
-        cmd2 := exec.Command(cur_dir+"/VMXwebcam2",os.Args[1])
+        cmd2 := exec.Command(cur_dir+"/.VMXwebcam",os.Args[1])
         stdin_pipe,_ := cmd2.StdinPipe()
         stdout_pipe,_ := cmd2.StdoutPipe()
 
@@ -61,8 +61,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf(r.RemoteAddr+" "+r.UserAgent()+"\n")
 		my_channel <- 1
-		//log.Printf(r.RemoteAddr+" "+r.UserAgent()+"Locked\n")
-		result_file := "/tmp/VMXwebcam."+strconv.Itoa(counter)+".jpg"
+		result_file := "/tmp/VMXwebcam."+strconv.Itoa(rand.Intn(50000))+"_"+strconv.Itoa(counter)+".jpg"
 		_,_ = stdin_pipe.Write([]byte(result_file+"\n"))
 		_,_ = stdout_pipe.Read(b1)
 		dat, _ := ioutil.ReadFile(result_file)
@@ -76,7 +75,7 @@ func main() {
 		counter = counter + 1
 		_ = os.Remove(result_file)
                 <-my_channel                           
-		//log.Printf(r.RemoteAddr+" "+r.UserAgent()+"Unlocked\n")
+
 	})
 	log.Fatal(http.Serve(listener, nil))
 }
